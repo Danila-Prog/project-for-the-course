e40b7c0d2f779cda67f3f71c7d04c84b13213304
Danila Korotin <korotin.dravestil@gmail.com>
2025-06-10 18:10:34 +0400

    chore: 8 попытка деплоя

--- src/page/AccountPage/AccountPage.tsx
+++ src/page/AccountPage/AccountPage.tsx
@@ -1,23 +1,23 @@
-// "use client";
+"use client";
 
-// import UserLogistician from "./ui/UserLogistician/UserLogistician";
-// import { useEffect, useState } from "react";
-// import UserDriver from "./ui/UserDriver";
-import { Header } from "../../widgets/Header/ui/Header";
+import UserLogistician from "./ui/UserLogistician/UserLogistician";
+import { useEffect, useState } from "react";
+import UserDriver from "./ui/UserDriver";
+import { Header } from "@/widgets/Header";
 
 export default function AccountPage() {
-  // const [roleId, setRoleId] = useState<string | null>(null);
+  const [roleId, setRoleId] = useState<string | null>(null);
 
-  // useEffect(() => {
-  //   setRoleId(localStorage.getItem("roleId") ?? "");
-  // }, []);
+  useEffect(() => {
+    setRoleId(localStorage.getItem("roleId") ?? "");
+  }, []);
 
   return (
     <div className="w-[75%] mx-auto">
       <Header />
 
-      {/* {Number(roleId) === 1 && <UserDriver />}
-      {Number(roleId) === 2 && <UserLogistician />} */}
+      {Number(roleId) === 1 && <UserDriver />}
+      {Number(roleId) === 2 && <UserLogistician />}
     </div>
   );
 }

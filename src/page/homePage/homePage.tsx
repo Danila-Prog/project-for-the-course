f7c4b8ab2cc1eb9fafeeafbba530b04616fc7129
Danila Korotin <korotin.dravestil@gmail.com>
2025-06-10 17:53:53 +0400

    chore: 7 попытка

--- src/page/homePage/homePage.tsx
+++ src/page/homePage/homePage.tsx
@@ -1,33 +1,33 @@
-"use client";
+// "use client";
 
 import { FooterHome } from "@/page/HomePage/ui/Footer";
 import { ConnectionBlock } from "./ui/ConnectionBlock";
 import { Main } from "@/page/HomePage/ui/Main";
 import { Header } from "@/widgets/Header";
-import { useEffect, useState } from "react";
-import { AccountPage } from "../AccountPage";
+// import { useEffect, useState } from "react";
+// import { AccountPage } from "../AccountPage";
 
 export default function HomePage() {
-  const [userId, setUserId] = useState<string | null>("");
+  // const [userId, setUserId] = useState<string | null>("");
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setUserId(localStorage.getItem("userId") ?? "");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     setUserId(localStorage.getItem("userId") ?? "");
+  //   }
+  // }, []);
 
   return (
     <>
-      {userId && <AccountPage />}
+      {/* {userId && <AccountPage />} */}
 
-      {!userId && (
-        <div className="w-[75%] mx-auto">
-          <Header />
-          <Main />
-          <ConnectionBlock />
-          <FooterHome />
-        </div>
-      )}
+      {/* {!userId && ( */}
+      <div className="w-[75%] mx-auto">
+        <Header />
+        <Main />
+        <ConnectionBlock />
+        <FooterHome />
+      </div>
+      {/* )} */}
     </>
   );
 }
